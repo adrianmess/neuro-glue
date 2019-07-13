d55bd91789b4046d48834cc72826af4d91552555
Adrian Messado <adrian.messado@gmail.com>
2019-07-13 14:47:45 -0700

    add event handling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import CardsList from './CardsList';
 import './App.css';
 
 class App extends React.Component {
-  addCard() {
-    alert("Hey!");
+  cardFrontInput = React.createRef();
+  cardBackInput = React.createRef();
+
+  addCard = event => {
+    // stop form from submitting
+    event.preventDefault();
+    //get text from input
+    console.log(this.cardFrontInput.current.value);
+    console.log(this.cardBackInput.current.value);
+
   }
   render() {
     return (
@@ -12,8 +20,20 @@ class App extends React.Component {
 
         <form className="cards-list-main" onSubmit={this.addCard}>
           <h2>Create Card</h2>
-          <input className="card-front" type="text" placeholder="Front"></input>
-          <input className="card-back" type="text" placeholder="Back"></input>
+          <input
+          className="card-front"
+          type="text"
+          placeholder="Front"
+          ref={this.cardFrontInput}
+          />
+
+
+          <input
+          className="card-back"
+          type="text"
+          placeholder="Back"
+          ref={this.cardBackInput}
+          />
           <button className="button" type="submit">Add</button>
         </form>
         <CardsList/>
