6452b41b1eeb6b24754657915de58abe7bc9c926
Adrian Messado <adrian.messado@gmail.com>
2019-07-13 14:19:22 -0700

    add cardlist component, add event button eventhandler, add handle click function

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardsList from './CardsList';
 import './App.css';
 
-function App() {
-  return (
-    <>
+class App extends React.Component {
+  handleClick() {
+    alert("Hey!");
+  }
+  render() {
+    return (
+      <>
 
-      <form className="cards-list-main">
-        <h2>Create Card</h2>
-        <input className="card-front" type="text" placeholder="Front"></input>
-        <input className="card-back" type="text" placeholder="Back"></input>
-        <button type="submit">Add</button>
-      </form>
+        <form className="cards-list-main">
+          <h2>Create Card</h2>
+          <input className="card-front" type="text" placeholder="Front"></input>
+          <input className="card-back" type="text" placeholder="Back"></input>
+          <button className="button" type="submit" onClick={this.handleClick}>Add</button>
+        </form>
+        <CardsList/>
 
-    </>
-  );
+      </>
+    );
+  }
+
 }
 
 export default App;
