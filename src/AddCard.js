1ba23587f5259b3302c80d8c1f6ae82d9f4004a8
Adrian Messado <adrian.messado@gmail.com>
2019-07-13 14:52:19 -0700

    move card form from App component to AddCard component, Add AddCard component to App component

--- src/AddCard.js
+++ src/AddCard.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
 class AddCard extends React.Component{
-	return() {
-		render(
+	render() {
+		return(
 			<>
+				<form className="cards-list-main" onSubmit={this.addCard}>
+					<h2>Create Card</h2>
+					<input
+						className="card-front"
+						type="text"
+						placeholder="Front"
+						ref={this.cardFrontInput}
+					/>
+
+
+					<input
+						className="card-back"
+						type="text"
+						placeholder="Back"
+						ref={this.cardBackInput}
+					/>
+					<button className="button" type="submit">Add</button>
+				</form>
 			</>
 		)
 	}
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardsList from './CardsList';
+import AddCard from './AddCard';
 import './App.css';
 
 class App extends React.Component {
@@ -17,27 +18,8 @@ class App extends React.Component {
   render() {
     return (
       <>
-
-        <form className="cards-list-main" onSubmit={this.addCard}>
-          <h2>Create Card</h2>
-          <input
-          className="card-front"
-          type="text"
-          placeholder="Front"
-          ref={this.cardFrontInput}
-          />
-
-
-          <input
-          className="card-back"
-          type="text"
-          placeholder="Back"
-          ref={this.cardBackInput}
-          />
-          <button className="button" type="submit">Add</button>
-        </form>
         <CardsList/>
-
+        <AddCard/>
       </>
     );
   }
