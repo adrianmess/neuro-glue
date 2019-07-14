f99b68449dc02d7396c763bed80f262cd124f50c
Adrian Messado <adrian.messado@gmail.com>
2019-07-13 19:12:31 -0700

    import Card.css, add remove button

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Card.css';
 
 class Card extends React.Component {
 	render() {
 		const { front, back} = this.props.details;
 		return (
 			<>
-			<div>
-			<p>{front}</p>
+			<div id="card-container">
+			<div id="card-front">{front}</div>
+			<div id="card-back">{back}</div>
 			</div>
+			<button id="card-button-remove">remove</button>
 			</>
 		)
 	}
