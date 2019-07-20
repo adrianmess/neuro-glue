f38f628a9b788dd9f3f189a4f0ddc5a8a0bc3010
Adrian Messado <adrian.messado@gmail.com>
2019-07-20 16:59:43 -0700

    rename EditCard to CardEditor, import ReactQuill, Add constructor, Add ReactQuill component

---
+++ src/CardEditor.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import './CardEditor.css';
+
+class CardEditor extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { text: '' } // You can also pass a Quill Delta here
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(value) {
+		this.setState({ text: value })
+	}
+	render(){
+		return(
+			<>
+			<div id="reactQuill-container">
+					<ReactQuill
+						value={this.state.text}
+						onChange={this.handleChange
+						}>
+						<div className="my-editing-area" />
+					</ReactQuill>
+			</div>
+
+			</>
+		)
+	}
+}
+
+export default CardEditor;
