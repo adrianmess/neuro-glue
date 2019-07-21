a7d58b38dd62d24e15e39ef6199081e4642b0aab
Adrian Messado <adrian.messado@gmail.com>
2019-07-21 11:49:42 -0700

    create css for FlashcardEditorMain, place components in divs, add addCard prop to CardEditor, Style as needed

--- src/FlashCardEditorMain.js
+++ src/FlashCardEditorMain.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import CardsList from './CardsList';
 import CardEditor from './CardEditor';
+import './FlashCardEditorMain.css';
 
 
 class FlashCardEditorMain extends React.Component{
 	render(){
 		return(
 			<>
-			<CardsList
-				cards={this.props.cards}
-				deleteCard={this.props.deleteCard}
-			/>
-			<CardEditor
-					cards={this.props.cards}
-			/>
+			<div id="cardList">
+					<CardsList
+						cards={this.props.cards}
+						deleteCard={this.props.deleteCard}
+					/>
+			</div>
+			<div id="cardEditor">
+					<CardEditor
+						cards={this.props.cards}
+						addCard={this.props.addCard}
+					/>
+			</div>
+
 
 			</>
 		);
