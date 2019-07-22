b967fe96107bcf8ca169d47ddb316b228f307689
Adrian Messado <adrian.messado@gmail.com>
2019-07-21 20:27:57 -0700

    add constructor, pass props, create cardSelected function to trigger setCardActive prop function

--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,26 @@ import React from 'react';
 import renderHTML from 'react-render-html';
 import './Card.css';
 
+
+
 class Card extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.cardSelected = this.cardSelected.bind(this);
+	}
+
+	cardSelected(){
+		this.props.setCardActive(this.props.index);
+	}
+
 	render() {
 		const { front, back} = this.props.card;
 		return (
 			<>
 			<div id="card-container"
 			onClick={() => this.props.selectCard(this.props.index)}
+			onClick={this.cardSelected}
 			>
 			<div id="card-front" name="front">
 				{renderHTML(front)}
