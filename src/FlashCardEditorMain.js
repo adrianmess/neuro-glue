8792f32167b64039b731c40c6da95a4e6f7b29d4
Adrian Messado <adrian.messado@gmail.com>
2019-07-27 02:48:12 -0700

    Move local state to App state, remove all functions, remove unused props/functions, pass new props

--- src/FlashCardEditorMain.js
+++ src/FlashCardEditorMain.js
@@ -5,28 +5,6 @@ import './FlashCardEditorMain.css';
 
 
 class FlashCardEditorMain extends React.Component{
-	constructor(props){
-		super(props);
-		this.state={
-			whichItemActive: null,
-			activeItemCardIndex: '',
-		}
-
-		this.setCardActive = this.setCardActive.bind(this);
-		this.setEditorActive = this.setEditorActive.bind(this);
-	}
-
-	setCardActive(index){
-		this.setState({
-			whichItemActive: 'card',
-			activeItemCardIndex: index
-		})
-	}
-
-	setEditorActive(){
-		this.setState({ whichItemActive: 'editor'})
-	}
-
 
 	render(){
 		return(
@@ -36,9 +14,6 @@ class FlashCardEditorMain extends React.Component{
 						cards={this.props.cards}
 						deleteCard={this.props.deleteCard}
 						selectCard={this.props.selectCard}
-						selectedCardIndex={this.props.selectedCardIndex}
-						whichItemActive={this.state.whichItemActive}
-						setCardActive={this.setCardActive}
 					/>
 			</div>
 			<div id="cardEditor">
@@ -46,10 +21,8 @@ class FlashCardEditorMain extends React.Component{
 						cards={this.props.cards}
 						addCard={this.props.addCard}
 						updateCard={this.props.updateCard}
-						cardSelected={this.state.cardSelected}
-						whichItemActive={this.state.whichItemActive}
-						activeItemCardIndex={this.state.activeItemCardIndex}
-						setEditorActive={this.setEditorActive}
+						selectedCard={this.props.selectedCard}
+						selectedCardIndex={this.props.selectedCardIndex}
 					/>
 			</div>
 			</>
