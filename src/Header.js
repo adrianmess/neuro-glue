2a31501caa4a3074e183937c43557367825f7aee
Adrian Messado <adrian.messado@gmail.com>
2019-07-28 00:44:32 -0700

    Connect to firebase auth and set to authProvider, then pass authData to authHandler

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Login from './Login';
+import firebase from 'firebase';
+import { firebaseApp } from './firebase';
 
 class Header extends React.Component{
-	authenticate = () => {
-		alert('yeah')
+
+	authHandler = async (authData) => {
+		console.log(authData)
 	}
+	authenticate = provider => {
+		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+		firebaseApp
+		.auth()
+		.signInWithPopup(authProvider)
+		.then(this.authHandler);
+	};
 	render(){
 		return(
 			<>
