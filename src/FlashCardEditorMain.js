b46616cb2313f3eafa98c9ce0be15cd746cc8ee0
Adrian Messado <adrian.messado@gmail.com>
2019-07-27 17:48:07 -0700

    add input field to contain the title of the flashcard set, add function to update local state with cardTitleSet name

--- src/FlashCardEditorMain.js
+++ src/FlashCardEditorMain.js
@@ -5,27 +5,51 @@ import './FlashCardEditorMain.css';
 
 
 class FlashCardEditorMain extends React.Component{
+	constructor(props){
+		super(props);
+			this.state= {
+				cardSetTitle: 'sds'
+			}
+	}
+
+handleChange = event => {
+	this.setState({
+		cardSetTitle: event.target.value
+	})
+}
 
 	render(){
 		return(
 			<>
-			<div id="cardList">
-					<CardsList
-						cards={this.props.cards}
-						deleteCard={this.props.deleteCard}
-						selectCard={this.props.selectCard}
-					/>
+			<div id="flashCardEditorContainer">
+
+				<p>
+					<input
+					placeholder= "FlashCard Set Title"
+					type= "text"
+					onChange={this.handleChange}
+					value={this.state.cardSetTitle}></input>
+				</p>
+
+					<div id="cardList">
+						<CardsList
+							cards={this.props.cards}
+							deleteCard={this.props.deleteCard}
+							selectCard={this.props.selectCard}
+						/>
+					</div>
+					<div id="cardEditor">
+						<CardEditor
+							cards={this.props.cards}
+							addCard={this.props.addCard}
+							updateCard={this.props.updateCard}
+							newCard={this.props.newCard}
+							selectedCard={this.props.selectedCard}
+							selectedCardIndex={this.props.selectedCardIndex}
+						/>
+					</div>
 			</div>
-			<div id="cardEditor">
-					<CardEditor
-						cards={this.props.cards}
-						addCard={this.props.addCard}
-						updateCard={this.props.updateCard}
-						newCard={this.props.newCard}
-						selectedCard={this.props.selectedCard}
-						selectedCardIndex={this.props.selectedCardIndex}
-					/>
-			</div>
+
 			</>
 		);
 	}
