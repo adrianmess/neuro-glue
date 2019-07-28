4a5144820850ff57d10ed0b226d98dc4fcfc7d93
Adrian Messado <adrian.messado@gmail.com>
2019-07-28 02:48:18 -0700

    add constructor, change const to retrieve user uid with await, pass userID as to userLogin prop

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Login from './Login';
 import firebase from 'firebase';
-import { firebaseApp } from './firebase';
+import base, { firebaseApp } from './firebase';
 
 class Header extends React.Component{
+	constructor(){
+		super()
+	}
 
 	authHandler = async (authData) => {
-		const user = await base.fetch(this.props.userId, { context: this });
+		const userID = await authData.user.uid;
+		this.props.userLogin(userID);
+		// const user = await base.fetch(this.props.userId, { context: this });
+
+		// if (!user.owner) {
+		// 	await base.post(`${this.props.userId}/owner`, {
+		// 		data: authData.user.uid
+		// 	});
+		// }
 	}
 	authenticate = provider => {
 		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
