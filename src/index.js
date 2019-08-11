64f74e8dfd71e81438d3875833a2d6fe991c73c4
Adrian Messado <adrian.messado@gmail.com>
2019-08-11 03:02:25 -0700

    remove React Router from index.js and replace with App, render App

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import Router from './Router';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-render(<Router/>, document.getElementById('root'));
+render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
