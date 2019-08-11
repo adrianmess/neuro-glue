4c647568273d4665d1fe4b426bdbb758be0b8156
Adrian Messado <adrian.messado@gmail.com>
2019-08-11 03:10:42 -0700

    Create span with router Link inside, set onClick function to span tag to execute setCardTitile, pass event, cardTitle and cardSetID. Link based on the cardTitle chosen

--- src/FlashCardSetsByTitle.js
+++ src/FlashCardSetsByTitle.js
@@ -3,30 +3,27 @@ import {Link} from 'react-router-dom';
 
 class FlashCardSetsByTitle extends React.Component{
 
-
-	setCurrentCardSetTitle(event, cardTitle){
+	setCardTitle(event, cardTitle, cardSetID){
 		event.preventDefault();
-		this.props.setCurrentCardSetTitle(cardTitle);
+		this.props.setCurrentCardSetTitle(cardTitle, cardSetID);
+
 	}
 
 	render(){
 		const cards = this.props.cards;
 		const cardTitles = Object.keys(cards).map(cardSetID => [cardSetID, cards[cardSetID].CardSetTitle]);
-
-
 		return(
 			<>
 			<div>
 					{cardTitles.map((cardTitles) =>
 					<div>
-						<Link
-							to={`/FlashCardTest/${cardTitles[1]}`}
-						>
-								<span onCLick={event => this.setCurrentCardSetTitle(event, cardTitles[1])}>
-									{cardTitles[1]}
-							</span>
+							<span onClick={event => this.setCardTitle(event, cardTitles[1], cardTitles[0])}>
+								<Link
+									to={`/FlashCardTest/${cardTitles[1]}`}
+								> {cardTitles[1]}
+								</Link>
+						</span>
 
-						</Link>
 					</div>
 					)}
 			</div>
