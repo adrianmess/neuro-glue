4940cbad5009e418c5f73afb6ebe21443513332b
Adrian Messado <adrian.messado@gmail.com>
2019-08-17 02:44:17 -0700

    prettify document, add unique key attribute to div

--- src/FlashCardSetsByTitle.js
+++ src/FlashCardSetsByTitle.js
@@ -1,35 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-class FlashCardSetsByTitle extends React.Component{
+class FlashCardSetsByTitle extends React.Component {
+  setCardTitle(event, cardTitle, cardSetID) {
+    event.preventDefault();
+    this.props.setCurrentCardSetTitle(cardTitle, cardSetID);
+  }
 
-	setCardTitle(event, cardTitle, cardSetID){
-		event.preventDefault();
-		this.props.setCurrentCardSetTitle(cardTitle, cardSetID);
-
-	}
-
-	render(){
-		const cards = this.props.cards;
-		const cardTitles = Object.keys(cards).map(cardSetID => [cardSetID, cards[cardSetID].CardSetTitle]);
-		return(
-			<>
-			<div>
-					{cardTitles.map((cardTitles) =>
-					<div>
-							<span onClick={event => this.setCardTitle(event, cardTitles[1], cardTitles[0])}>
-								<Link
-									to={`/FlashCardTest/${cardTitles[1]}`}
-								> {cardTitles[1]}
-								</Link>
-						</span>
-
-					</div>
-					)}
-			</div>
-			</>
-		)
-	}
+  render() {
+    const cards = this.props.cards;
+    const cardTitles = Object.keys(cards).map(cardSetID => [
+      cardSetID,
+      cards[cardSetID].CardSetTitle
+    ]);
+    console.log(cardTitles);
+    return (
+      <>
+        <div>
+          {cardTitles.map(cardTitles => (
+            <div key={cardTitles[0]}>
+              <span
+                onClick={event =>
+                  this.setCardTitle(event, cardTitles[1], cardTitles[0])
+                }
+              >
+                <br />
+                <Link
+                  to={`/FlashCardTest/${cardTitles[1]}`}
+                >
+                  {cardTitles[1]}
+                </Link>
+              </span>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 
-export default FlashCardSetsByTitle
+export default FlashCardSetsByTitle;
