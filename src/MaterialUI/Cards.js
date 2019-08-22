316e936c0c3c4bb600928d275e9d4f3e2cac3a9a
Adrian Messado <adrian.messado@gmail.com>
2019-08-21 21:04:46 -0700

    pure-react-carousel moved to FlashCardTest component, thumb-up/down FlashCardTest component, removed unused material ui modules, removed commented out code.

---
+++ src/MaterialUI/Cards.js
@@ -0,0 +1,95 @@
+import React, { useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Collapse from "@material-ui/core/Collapse";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+
+
+import renderHTML from "react-render-html";
+
+import ReactCardFlip from "react-card-flip";
+
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
+
+export default function Cards(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [isFlipped, setFlipped] = React.useState(false);
+
+  useEffect(() => {
+    let currentSlide = props.activeSlideState.current.state.currentSlide;
+    props.setActiveCard(currentSlide);
+  });
+
+  function handleClick(e) {
+    e.preventDefault();
+    setFlipped(!isFlipped);
+  }
+
+
+
+  return (
+    <>
+      <ReactCardFlip
+        isFlipped={isFlipped}
+        flipDirection="horizontal"
+        cardZIndex="1"
+      >
+        <div
+          id="cardFront"
+          key="front"
+          onClick={event => handleClick(event, props.cardindex)}
+        >
+          <Card className={classes.card}>
+            <CardContent>{renderHTML(props.card.front)}</CardContent>
+          </Card>
+        </div>
+
+        <div
+          id="cardBack"
+          key="back"
+          onClick={event => handleClick(event, props.cardindex)}
+        >
+          <Card className={classes.card}>
+            <CardContent>{renderHTML(props.card.back)}</CardContent>
+          </Card>
+        </div>
+      </ReactCardFlip>
+      {!isFlipped ? null : (
+        <>
+
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>References:</Typography>
+            </CardContent>
+          </Collapse>
+        </>
+      )}
+    </>
+  );
+}
