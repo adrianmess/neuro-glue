39a76ec7634e347aa0145a03cb2cdf91b13176be
Adrian Messado <adrian.messado@gmail.com>
2019-08-24 15:58:19 -0700

    prettier, pass cardSetTitle as prop to CardEditor

--- src/FlashCardEditorMain.js
+++ src/FlashCardEditorMain.js
@@ -1,140 +1,150 @@
-import React from 'react';
-import CardEditorList from './CardEditorList';
-import CardEditor from './CardEditor';
-import { firestore } from './firebase';
+import React from "react";
+import CardEditorList from "./CardEditorList";
+import CardEditor from "./CardEditor";
+import { firestore } from "./firebase";
 
-import './FlashCardEditorMain.css';
+import "./FlashCardEditorMain.css";
 
+class FlashCardEditorMain extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allCards: {},
+      cardSetTitle: "",
+      cardSetDate: Date.now(),
+      cardSet: {},
+      cardFront: "test-CardFront",
+      cardBack: "test-CardBack",
+      cardNote: "test-Notes"
+    };
+    this.createNewCardSet = this.createNewCardSet.bind(this);
+  }
 
+  //grab current card set from firebase and store locally in state
 
-class FlashCardEditorMain extends React.Component{
-	constructor(props){
-		super(props);
-			this.state= {
-				allCards:{},
-				cardSetTitle: '',
-				cardSetDate: Date.now(),
-				cardSet:{},
-				cardFront: 'test-CardFront',
-				cardBack: 'test-CardBack',
-				cardNote: 'test-Notes',
-			}
-			this.createNewCardSet = this.createNewCardSet.bind(this);
-	}
+  // setDocToState = (allCards) =>{
+  // 	this.setState({
+  // 		allCards: allCards
+  // 	})
+  // }
 
+  handleChange = event => {
+    this.setState({
+      cardSetTitle: event.target.value
+    });
+    // this.props.createTitle(this.state.cardCategory)
+  };
 
+  selectedCards() {}
 
-//grab current card set from firebase and store locally in state
+  createNewCardSet = () => {
+    const cardSetDate = this.state.cardSetDate;
+    // const Cards
+    const newCardSet = {
+      [`cardSet${cardSetDate}`]: {
+        CardSetTitle: `${this.state.cardSetTitle}`,
+        Category: `${this.state.cardSetCategory}`,
+        Cards: JSON.stringify(`${this.state.cardSet}`)
+      }
+    };
 
+    console.log(this.state.cardSet);
+    this.props.addOrUpdateCard(newCardSet);
+  };
 
+  addCard = card => {
+    console.log(card);
+    const cards = { ...this.state.cardSet };
+    cards[`card${Date.now()}`] = card;
 
-// setDocToState = (allCards) =>{
-// 	this.setState({
-// 		allCards: allCards
-// 	})
-// }
+    this.setState({ cardSet: cards });
+  };
 
-handleChange = event => {
-	this.setState({
-		cardSetTitle: event.target.value
-	})
-	// this.props.createTitle(this.state.cardCategory)
-}
+  render() {
+    return (
+      <>
+        <div id="flashCardEditorContainer">
+          <p>
+            <input
+              placeholder="FlashCard Set Title"
+              type="text"
+              onChange={this.handleChange}
+              value={this.state.cardSetTitle}
+            />
+          </p>
+          <span />
 
+          <button>Delete Set</button>
+          <button
+            // onClick={this.props.saveCurrentCardSet}
+            onClick={this.createNewCardSet}
+          >
+            Save
+          </button>
 
-selectedCards(){
-}
+          <div id="cardList">
+            <CardEditorList
+              cards={this.props.cards}
+              deleteCard={this.props.deleteCard}
+              // selectCard={this.props.selectCard}
+              // cardCategory={this.props.cardCategory}
 
-createNewCardSet =()=>{
-	const cardSetDate = this.state.cardSetDate;
-	// const Cards
-	const newCardSet = {
-		[`cardSet${cardSetDate}`]: {
-			CardSetTitle: `${this.state.cardSetTitle}`,
-			Category: `${this.state.cardSetCategory}`,
-			Cards: JSON.stringify(`${this.state.cardSet}`)
-		}
-	}
+              currentCardSet={this.props.currentCardSet}
+              currentCardSetID={this.props.currentCardSetID}
+              currentCardSetCategory={this.props.currentCardSetCategory}
+              currentCardSetTitle={this.props.currentCardSetTitle}
+              currentCardSetScores={this.props.currentCardSetScores}
+              currentCardSetCards={this.props.currentCardSetCards}
+              currentCardSetCardsCardDate={
+                this.props.currentCardSetCardsCardDate
+              }
+              currentCardSetCardsCardFront={
+                this.props.currentCardSetCardsCardFront
+              }
+              currentCardSetCardsCardFack={
+                this.props.currentCardSetCardsCardFack
+              }
+              currentCardSetCardsCardFotes={
+                this.props.currentCardSetCardsCardFotes
+              }
+            />
+          </div>
+          <div id="cardEditor">
+            <CardEditor
+              cards={this.props.cards}
+              addCard={this.addCard}
+              updateCard={this.props.updateCard}
+              newCard={this.props.newCard}
+              newCardSet={this.newCardSet}
+              selectedCard={this.props.selectedCard}
+			  selectedCardIndex={this.props.selectedCardIndex}
 
-	console.log(this.state.cardSet);
-	this.props.addOrUpdateCard(newCardSet)
-}
+			  cardSetTitle={this.state.cardSetTitle}
 
-addCard = card =>{
-	console.log(card);
-	const cards = {...this.state.cardSet}
-	cards[`card${Date.now()}`] = card;
-
-	this.setState({cardSet: cards});
-}
-
-
-
-	render(){
-		return(
-			<>
-			<div id="flashCardEditorContainer">
-				<p>
-					<input
-					placeholder= "FlashCard Set Title"
-					type= "text"
-					onChange={this.handleChange}
-					value={this.state.cardSetTitle}></input>
-				</p><span></span>
-
-				<button >Delete Set</button>
-					<button
-					// onClick={this.props.saveCurrentCardSet}
-					onClick={this.createNewCardSet}
-					>Save</button>
-
-					<div id="cardList">
-						<CardEditorList
-							cards={this.props.cards}
-							deleteCard={this.props.deleteCard}
-							// selectCard={this.props.selectCard}
-							// cardCategory={this.props.cardCategory}
-
-							currentCardSet={this.props.currentCardSet}
-							currentCardSetID={this.props.currentCardSetID}
-							currentCardSetCategory={this.props.currentCardSetCategory}
-							currentCardSetTitle={this.props.currentCardSetTitle}
-							currentCardSetScores={this.props.currentCardSetScores}
-							currentCardSetCards={this.props.currentCardSetCards}
-							currentCardSetCardsCardDate={this.props.currentCardSetCardsCardDate}
-							currentCardSetCardsCardFront={this.props.currentCardSetCardsCardFront}
-							currentCardSetCardsCardFack={this.props.currentCardSetCardsCardFack}
-							currentCardSetCardsCardFotes={this.props.currentCardSetCardsCardFotes}
-						/>
-					</div>
-					<div id="cardEditor">
-						<CardEditor
-							cards={this.props.cards}
-							addCard={this.addCard}
-							updateCard={this.props.updateCard}
-							newCard={this.props.newCard}
-							newCardSet={this.newCardSet}
-							selectedCard={this.props.selectedCard}
-							selectedCardIndex={this.props.selectedCardIndex}
-
-							userID={this.props.userID}
-
-							currentCardSetID={this.props.currentCardSetID}
-							currentCardSetCategory={this.props.currentCardSetCategory}
-							currentCardSetTitle={this.props.currentCardSetTitle}
-							currentCardSetScores={this.props.currentCardSetScores}
-							currentCardSetCards={this.props.currentCardSetCards}
-							currentCardSetCardsCardDate={this.props.currentCardSetCardsCardDate}
-							currentCardSetCardsCardFront={this.props.currentCardSetCardsCardFront}
-							currentCardSetCardsCardFack={this.props.currentCardSetCardsCardFack}
-							currentCardSetCardsCardFotes={this.props.currentCardSetCardsCardFotes}
-						/>
-					</div>
-			</div>
-
-			</>
-		);
-	}
+              userID={this.props.userID}
+              currentCardSetID={this.props.currentCardSetID}
+              currentCardSetCategory={this.props.currentCardSetCategory}
+              currentCardSetTitle={this.props.currentCardSetTitle}
+              currentCardSetScores={this.props.currentCardSetScores}
+              currentCardSetCards={this.props.currentCardSetCards}
+              currentCardSetCardsCardDate={
+                this.props.currentCardSetCardsCardDate
+              }
+              currentCardSetCardsCardFront={
+                this.props.currentCardSetCardsCardFront
+              }
+              currentCardSetCardsCardFack={
+                this.props.currentCardSetCardsCardFack
+              }
+              currentCardSetCardsCardFotes={
+                this.props.currentCardSetCardsCardFotes
+              }
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default FlashCardEditorMain;
