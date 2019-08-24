d80581ce91e91bb7a2bf4bcf563425689b91088f
Adrian Messado <adrian.messado@gmail.com>
2019-08-24 13:55:51 -0700

    add Create Flash Cards link

--- src/Header.js
+++ src/Header.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import SimpleMenu from './MaterialUI/CardCategoryMenu';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import SimpleMenu from "./MaterialUI/CardCategoryMenu";
+import "./Header.css";
 
-class Header extends React.Component{
-	constructor(){
-		super()
-		this.state = {
-			selectedCardCategory: ''
-		}
-	}
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedCardCategory: ""
+    };
+  }
 
-	render(){
-		return(
-			<>
-			<div>
-				<Link to="/Categories" className="headerLinks">Categories</Link>
-				<Link to="/FlashCards" className="headerLinks">Flash Cards</Link>
-			</div>
-				{/* <SimpleMenu
+  render() {
+    return (
+      <>
+        <div>
+          <Link to="/Categories" className="headerLinks">
+            Categories
+          </Link>
+          <Link to="/FlashCards" className="headerLinks">
+            Flash Cards
+          </Link>
+          <Link to="/FlashCardEditor" className="headerLinks">
+            Create Flash Cards
+          </Link>
+        </div>
+        {/* <SimpleMenu
 				cards={this.props.cards}
 				select={this.props.selectCardCategory}>
 				</SimpleMenu> */}
-			</>
-		)
-	}
+      </>
+    );
+  }
 }
 
-export default Header
+export default Header;
