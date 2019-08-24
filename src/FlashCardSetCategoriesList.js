d8eb56147d397d547f762414b32623cf0c7bccc4
Adrian Messado <adrian.messado@gmail.com>
2019-08-24 13:06:09 -0700

    re-use setCardTitle function from FlashCardSetsByTitle component, import Link from react-router, Link to FlashCardTest using card Title in url, onclick run setCardTitle function and pass cardTitle and CardID

--- src/FlashCardSetCategoriesList.js
+++ src/FlashCardSetCategoriesList.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class FlashCardSetCategoriesList extends React.Component {
+  setCardTitle(event, cardTitle, cardSetID) {
+    event.preventDefault();
+    this.props.setCurrentCardSetTitle(cardTitle, cardSetID);
+  }
+
   render() {
     const { cards, selectedCardCategory } = this.props;
     const matchingCardArrays = Object.entries(cards).filter(keys => {
@@ -14,12 +20,17 @@ class FlashCardSetCategoriesList extends React.Component {
       cardSetTitleIDPair.push([cardSetID, cardSetTitle]);
     }
 
-
     return (
       <>
         {cardSetTitleIDPair.map(keys => (
           <div key={keys}>
-            {keys[1]}
+            <span
+              onClick={event =>
+                this.setCardTitle(event, keys[1], keys[0])
+              }
+            >
+              <Link to={`/FlashCardTest/${keys[1]}`}>{keys[1]}</Link>
+            </span>
           </div>
         ))}
       </>
