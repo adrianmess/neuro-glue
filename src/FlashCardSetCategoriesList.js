2077a10f7f8dee64461e95352d0f654b30058fad
Adrian Messado <adrian.messado@gmail.com>
2019-08-24 12:43:00 -0700

    loop over matchingCardArrays, set variables equal to carSetID and cardSetTitle, push ID and Title as pair in an Array to global array, within render function map through array and display CardSetTitles. Essentially display Card Set Title of matching categories

--- src/FlashCardSetCategoriesList.js
+++ src/FlashCardSetCategoriesList.js
@@ -3,12 +3,27 @@ import React from "react";
 class FlashCardSetCategoriesList extends React.Component {
   render() {
     const { cards, selectedCardCategory } = this.props;
-    const test = Object.entries(cards).filter(keys => {
+    const matchingCardArrays = Object.entries(cards).filter(keys => {
       return keys[1].Category === selectedCardCategory;
     });
 
-    console.log(test)
-    return <>{}</>;
+    const cardSetTitleIDPair = [];
+    for (let a = 0; a < matchingCardArrays.length; a++) {
+      const cardSetID = matchingCardArrays[a][0];
+      const cardSetTitle = matchingCardArrays[a][1].CardSetTitle;
+      cardSetTitleIDPair.push([cardSetID, cardSetTitle]);
+    }
+
+
+    return (
+      <>
+        {cardSetTitleIDPair.map(keys => (
+          <div key={keys}>
+            {keys[1]}
+          </div>
+        ))}
+      </>
+    );
   }
 }
 
