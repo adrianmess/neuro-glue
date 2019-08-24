40a1091d23d6ae928fc2a56db4dc0cdbeee754ca
Adrian Messado <adrian.messado@gmail.com>
2019-08-24 13:48:50 -0700

    add styling to header links

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import SimpleMenu from './MaterialUI/CardCategoryMenu';
+import './Header.css';
 
 class Header extends React.Component{
 	constructor(){
@@ -14,8 +15,8 @@ class Header extends React.Component{
 		return(
 			<>
 			<div>
-				<Link to="/Categories">Categories</Link>
-				<Link to="/FlashCards">Flash Cards</Link>
+				<Link to="/Categories" className="headerLinks">Categories</Link>
+				<Link to="/FlashCards" className="headerLinks">Flash Cards</Link>
 			</div>
 				{/* <SimpleMenu
 				cards={this.props.cards}
