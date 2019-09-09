ddecd7339ce0c36d9115b2a5dd84f37988cc5dfe
Adrian Messado <adrian.messado@gmail.com>
2019-09-08 18:46:50 -0700

    create two-tone material-ui TrashButton icon to be used within CardEditor cards list

---
+++ src/MaterialUI/DeleteTrashIcon-TwoTone.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
+import DeleteTwoToneIcon from '@material-ui/icons/DeleteTwoTone';
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
+import DeleteForeverRoundedIcon from '@material-ui/icons/DeleteForeverRounded';
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
+import DeleteForeverSharpIcon from '@material-ui/icons/DeleteForeverSharp';
+import ThreeDRotationIcon from '@material-ui/icons/ThreeDRotation';
+import FourKIcon from '@material-ui/icons/FourK';
+import ThreeSixtyIcon from '@material-ui/icons/ThreeSixty';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    color: theme.palette.text.primary,
+  },
+  icon: {
+    margin: theme.spacing(1),
+    fontSize: 32,
+  },
+}));
+
+export default function DeleteTwoTone() {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid item xs={4}>
+        <Typography>Two Tone</Typography>
+      </Grid>
+    </Grid>
+  );
+}
