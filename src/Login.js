22b450ee2c208217f9eaa1b892ed3e9dda33f67f
Adrian Messado <adrian.messado@gmail.com>
2019-09-21 12:45:52 -0700

    prettier code, add Google Auth button - comment out for now

--- src/Login.js
+++ src/Login.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import firebase from 'firebase/app';
-import { firebaseApp } from './firebase';
+import React from "react";
+import PropTypes from "prop-types";
+import firebase from "firebase/app";
+import { firebaseApp } from "./firebase";
 
+class Login extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+        this.props.setUserId(user.uid);
+      }
+    });
+  }
+  authHandler = async authData => {
+    const userID = await authData.user.uid;
+    this.props.setUserId(userID);
+    // const user = await base.fetch(this.props.userId, { context: this });
 
-class Login extends React.Component{
+    // if (!user.owner) {
+    // 	await base.post(`${this.props.userId}/owner`, {
+    // 		data: authData.user.uid
+    // 	});
+    // }
+  };
+  authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
 
-	componentDidMount(){
-		firebase.auth().onAuthStateChanged(user => {
-			if (user) {
-				this.authHandler({ user });
-				this.props.setUserId(user.uid)
-			}
-		})
-	}
-	authHandler = async (authData) =>{
-		const userID = await authData.user.uid;
-		this.props.setUserId(userID)
-		// const user = await base.fetch(this.props.userId, { context: this });
-
-		// if (!user.owner) {
-		// 	await base.post(`${this.props.userId}/owner`, {
-		// 		data: authData.user.uid
-		// 	});
-		// }
-	}
-	authenticate = provider => {
-		const authProvider = new firebase.auth[`${provider}AuthProvider`]();
-		firebaseApp
-			.auth()
-			.signInWithPopup(authProvider)
-			.then(this.authHandler);
-	};
-
-	render(){
-		return (
-		<nav className="login">
-			<button className="facebook"
-				onClick={() => this.authenticate('Facebook')}>Login</button>
-		</nav>
-		)
-	}
+  render() {
+    return (
+      <nav className="login">
+        <button
+          className="facebook"
+          onClick={() => this.authenticate("Facebook")}
+        >
+          Login In With Facebook
+        </button>
+        {/* <button
+          className="google"
+          onClick={() => this.authenticate("Google")}
+        >
+          Login In With Google
+        </button> */}
+      </nav>
+    );
+  }
 }
 
 // Login.propTypes = {
