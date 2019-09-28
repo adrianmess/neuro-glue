075ac48ce146d4f484d4080906c0cd278bb4acf5
Adrian Messado <adrian.messado@gmail.com>
2019-09-28 16:34:59 -0700

    add material-ui to FlashCardSetCategoriesList

--- src/FlashCardSetCategoriesList.js
+++ src/FlashCardSetCategoriesList.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import AssignmentIcon from "@material-ui/icons/Assignment";
 
 class FlashCardSetCategoriesList extends React.Component {
   setCardTitle(event, cardTitle, cardSetID) {
@@ -25,12 +32,20 @@ class FlashCardSetCategoriesList extends React.Component {
         <h2>Category: {selectedCardCategory}</h2>
         {cardSetTitleIDPair.map(keys => (
           <div key={keys[0]}>
-            <span
-              onClick={event =>
-                this.setCardTitle(event, keys[1], keys[0])
-              }
-            >
-              <Link to={`/FlashCardTest/${keys[1]}`}>{keys[1]}</Link>
+            <span onClick={event => this.setCardTitle(event, keys[1], keys[0])}>
+              <Link to={`/FlashCardTest/${keys[1]}`}>
+                <ListItem button>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <AssignmentIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+
+                  <ListItemText
+                    primary={keys[1]}
+                  />
+                </ListItem>
+              </Link>
             </span>
           </div>
         ))}
