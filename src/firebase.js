f1197c8225d04ce1fce9180ad33973d0bf224e63
Adrian Messado <adrian.messado@gmail.com>
2019-10-04 20:01:10 -0700

    move firebase config information to firebaseConfig.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,12 @@
 import Rebase from "re-base";
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';        // for authentication
-import 'firebase/storage';     // for storage
-import 'firebase/database';    // for realtime database
-import 'firebase/firestore';   // for cloud firestore
-// for cloud functions
+import firebase from "firebase/app";
+import "firebase/auth"; // for authentication
+import "firebase/storage"; // for storage
+import "firebase/database"; // for realtime database
+import "firebase/firestore"; // for cloud firestore
+import firebaseConfig from "./firebaseConfig";
 
-const firebaseApp = firebase.initializeApp({
-	apiKey: "AIzaSyDXYjVUgY1eZtnVXohdmsfv66vl_Sl2HVw",
-	authDomain: "neuro-glue.firebaseapp.com",
-	databaseURL: "https://neuro-glue.firebaseio.com",
-	projectId: "neuro-glue",
-	storageBucket: "neuro-glue.appspot.com",
-	messagingSenderId: "717836149705"
-});
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const firestore = firebaseApp.firestore();
 // const settings = { timestampsInSnapshots: true};
