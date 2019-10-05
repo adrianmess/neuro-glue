95c7183db09864b88ed58adc531e21788aadebd5
Adrian Messado <adrian.messado@gmail.com>
2019-10-05 10:27:45 -0700

    import SmallChart component, and component within Material UI list component and test

--- src/MaterialUI/FlashCardSetsByTitle.js
+++ src/MaterialUI/FlashCardSetsByTitle.js
@@ -11,6 +11,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import EditIcon from "@material-ui/icons/Edit";
 import AssignmentIcon from "@material-ui/icons/Assignment";
+import SmallChart from "../charts/smallChart";
 
 import "./FlashCardSetsByTitle.css";
 
@@ -100,6 +101,7 @@ export default function FlashCardSetsByTitle(props) {
                     </ListItem>
                   </Link>
                 </div>
+                <SmallChart />
                 <ListItemSecondaryAction>
                   <IconButton edge="end" aria-label="delete">
                     <Link to={"/FlashCardEditor"}>
