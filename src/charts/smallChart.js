ac8e7d14709328f82af5b3d04d9e9f0a1f73d4cc
Adrian Messado <adrian.messado@gmail.com>
2019-10-05 18:23:38 -0700

    change style of grid, grid will only show last five test scores or less. if there are no scores display a blank grid.

--- src/charts/smallChart.js
+++ src/charts/smallChart.js
@@ -5,63 +5,92 @@ import {
   XAxis,
   YAxis,
   CartesianGrid,
-  Tooltip,
-  Legend
 } from "recharts";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
 
 export default class Example extends PureComponent {
   static jsfiddleUrl = "https://jsfiddle.net/alidingling/exh283uh/";
 
   render() {
+    const { cards, currentCardSetID } = this.props;
+    const scores = cards[currentCardSetID]["Scores"];
+    const lastFiveScores = [];
+    const scoreBoolean = scores !== undefined;
+
+    if (scores !== undefined) {
+      if (scores.length > 5) {
+        for (let i = scores.length - 1; i >= scores.length - 5; i--) {
+          lastFiveScores.push(scores[i]);
+        }
+      } else {
+        for (let i = 0; i < scores.length; i++) {
+          lastFiveScores.push(scores[i]);
+        }
+      }
+    }
+
     return (
-      <LineChart width={120} height={40} data={data}>
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
-      </LineChart>
+      <div id="smallGraphContainer">
+        {scoreBoolean ? (
+          <div id="arrayWithData">
+            <LineChart width={120} height={40} data={lastFiveScores}>
+              <CartesianGrid
+                strokeDasharray="1 1"
+                horizontalPoints={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90]}
+                fillOpacity={1}
+              />
+              <YAxis
+                tick={false}
+                domain={[0, 100]}
+                // ticks={[25, 50, 75, 100]}
+                // tick={{ stroke: "red", strokeWidth: 1, fontSize: 0 }}
+                // tickSize={10}
+                // minTickGap={0.01}
+                hide
+              />
+              <XAxis tick={true} domain={[0, 8]} hide />
+              <Line
+                type="monotone"
+                dataKey={"scorePercentAsNum"}
+                stroke="#8884d8"
+                strokeWidth={1}
+                // animationDuration={0}
+                isAnimationActive={true}
+              />
+              {/* <Area dataKey="scorePercentAsNum" dot={true} /> */}
+            </LineChart>
+          </div>
+        ) : (
+          <div id="emptyArray">
+            {" "}
+            <LineChart width={120} height={40}>
+              <CartesianGrid
+                strokeDasharray="1 1"
+                horizontalPoints={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90]}
+                fillOpacity={1}
+              />
+              <YAxis
+                tick={false}
+                domain={[0, 100]}
+                // ticks={[25, 50, 75, 100]}
+                // tick={{ stroke: "red", strokeWidth: 1, fontSize: 0 }}
+                // tickSize={10}
+                // minTickGap={0.01}
+                hide
+              />
+              <XAxis tick={true} domain={[0, 8]} hide />
+              <Line
+                type="monotone"
+                dataKey={"scorePercentAsNum"}
+                stroke="#8884d8"
+                strokeWidth={1}
+                // animationDuration={0}
+                isAnimationActive={true}
+              />
+              {/* <Area dataKey="scorePercentAsNum" dot={true} /> */}
+            </LineChart>
+          </div>
+        )}
+      </div>
     );
   }
 }
