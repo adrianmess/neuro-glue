bbde344ac895c0caf409955dac53ca773756eff1
Adrian Messado <adrian.messado@gmail.com>
2019-10-06 17:52:37 -0700

    create function compnented based on react-select to be used to select multiple categories

---
+++ src/Components/react-select/multiple-animated.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { colourOptions } from "../data";
+
+const animatedComponents = makeAnimated();
+
+export default function AnimatedMulti(props) {
+  const [values, setValues] = React.useState({
+    category: "",
+    name: "",
+    categories: "",
+    value: "",
+    label: "",
+    selectedCategories: []
+  });
+
+  React.useEffect(() => {
+    const allCards = props.cards;
+    const categories = Object.keys(allCards).map(key => allCards[key].Category);
+    const allUniqueCategories = Array.from(new Set(categories));
+    // remove undefined from unique categories
+    const uniqueCategories = allUniqueCategories.filter(x => x !== undefined);
+
+    setValues(oldValues => ({
+      ...oldValues,
+      categories: uniqueCategories
+    }));
+
+    if (props.cardSetCategory !== ("" || null || undefined)) {
+      setValues(oldValues => ({
+        ...oldValues,
+        category: props.cardSetCategory
+      }));
+    }
+  }, [props.cardSetCategory, props.cards]);
+
+  function handleChange(inputValue, actionMeta) {
+    // console.log(`action: ${actionMeta.action}`);
+
+    if (inputValue === null) {
+      props.selectedCategories([]);
+    }
+    if (inputValue !== null) {
+      props.selectedCategories(inputValue);
+    }
+  }
+
+  const categoriesList = Object.values(values.categories)
+    .filter(x => x !== "")
+    .map(value => Object.assign({ value: value }, { label: value }));
+
+  return (
+    <Select
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      onChange={handleChange}
+      //   defaultValue={[colourOptions[4], colourOptions[5]]}
+      isMulti
+      options={categoriesList}
+    />
+  );
+}
