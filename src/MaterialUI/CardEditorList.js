cfb2ef862011cdfe2177cbca29bf0820dcf8b21a
Adrian Messado <adrian.messado@gmail.com>
2019-10-29 00:23:23 -0700

    add id selector to main div

--- src/MaterialUI/CardEditorList.js
+++ src/MaterialUI/CardEditorList.js
@@ -1,19 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import FolderIcon from "@material-ui/icons/Folder";
 import DeleteIcon from "@material-ui/icons/Delete";
 import renderHTML from "react-render-html";
 
@@ -63,7 +52,7 @@ export default function MaterialCardEditorList(props) {
 
   if (cardsList !== undefined) {
     return (
-      <div className={classes.root}>
+      <div className={classes.root} id="card_editor_list_root">
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
             <div className={classes.demo}>
